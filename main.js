f612e5701875feff5c2afbec9021e660e0fef0a6
Vistril <38549907+Vistril@users.noreply.github.com>
2019-09-19 16:59:10 -0600

    really dumb way to parse song lengths

--- main.js
+++ main.js
@@ -1,39 +1,58 @@
 const request = require('request');
 
 class ChorusAPI {
-    constructor() {
-        this.API =  {
-            URL: 'http://chorus.fightthe.pw/',
-            SEARCH: 'api/search?',
-            RANDOM: 'api/random',
-            COUNT: 'api/count'
+  constructor() {
+    this.API = {
+      URL: 'http://chorus.fightthe.pw/',
+      SEARCH: 'api/search?',
+      RANDOM: 'api/random',
+      COUNT: 'api/count'
+    }
+  }
+
+  search(type, query, resultCount) {
+    function colon(time) {
+      if (time.length == 4) {
+        parts = time.match(/.{1,2}/g);
+        rep = parts.join(':');
+      } else if (time.length == 3) {
+        time = '0' + time;
+        parts = time.match(/.{1,2}/g);
+        rep = parts.join(':').replace(/0/, '');
+      } else if (time.length == 2 || time.length == 1 && time != '0') {
+        time = '00' + time;
+        parts = time.match(/.{1,2}/g);
+        rep = parts.join(':');
+      } else {
+        rep = 'Something went wrong!';
+      }
+      return rep;
+    }
+    if (isNaN(resultCount)) throw new Error('You must input your resultCount as an integer');
+    let results = {};
+    let url = this.API.URL;
+    url += this.API.SEARCH + 'query=' + type + '=' + query;
+    request.get(url, (err, req, body) => {
+      let data = JSON.parse(body);
+      if (err) throw err;
+      for (let i = 0; i < resultCount - 1; i++) {
+        results[resultCount] = {
+          songName: data.songs[resultCount].name,
+          songArtist: data.songs[resultCount].artist,
+          songCharter: data.songs[resultCount].charter,
+          songYear: data.songs[resultCount].year,
+          songLengths: {
+            length: colon(data.songs[resultCount][8]),
+            effectiveLength: colon(data.songs[resultCount].effectiveLength)
+          }
         }
-    }
+      }
+    });
 
-    search(type, query, resultCount) {
-        if (isNaN(resultCount)) throw new Error('You must input your resultCount as an integer');
-        let results = {};
-        let url = this.API.URL;
-        url += this.API.SEARCH + 'query=' + type + '=' + query;
-        request.get(url, (err, req, body) => {
-            let data = JSON.parse(body);
-            if (err) throw err;
-            for (let i = 0; i < resultCount - 1; i++) {
-                results[resultCount] = {
-                    songName: data.songs[resultCount].name,
-                    songArtist: data.songs[resultCount].artist,
-                    songCharter: data.songs[resultCount].charter,
-                    songYear: data.songs[resultCount].year,
-                    songLengths: {
-                        length: data.songs[resultCount].length,
-                        effectiveLength: data.songs[resultCount].effectiveLength
-                    }
-                }
-            }
-        });
-
-        return results;
-    }
+    return results;
+  }
 }
 
 module.exports = ChorusAPI;
+
+colon('0');
