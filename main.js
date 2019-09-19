112ab21e6d329ad24271c411696447ab97a81a53
Vistril <38549907+Vistril@users.noreply.github.com>
2019-09-19 15:16:11 -0600

    search

---
+++ main.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+class ChorusAPI {
+    constructor() {
+        this.URL = 'http://chorus.fightthe.pw/';
+        this.search = 'api/search';
+        this.count = 'api/count';
+        this.random = 'api/random';
+    }
+
+    search(type, query, resultCount) {
+        let results = {};
+        let url = URL;
+        url += '/api/search?query=' + type + '=' + searchQuery;
+        request.get(url, (err, req, body) => {
+          for (let i = 0; i < resultCount; i++) {
+              results[resultCount] = {
+                  songName: body.songs[resultCount].name
+              }
+          }
+        });
+
+    }
+}
