269505580363ab2019c464c1ceead754081cf031
Vistril <38549907+Vistril@users.noreply.github.com>
2019-09-19 15:50:22 -0600

    capitalise constants

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ const request = require('request');
 class ChorusAPI {
     constructor() {
         this.URL = 'http://chorus.fightthe.pw/';
-        this._search = 'api/search?';
-        this.count = 'api/count';
-        this.random = 'api/random';
+        this.TYPE =  {
+            SEARCH: 'api/search?',
+            RANDOM: 'api/random',
+            COUNT: 'api/count'
+        }
     }
 
     search(type, query, resultCount) {
         if (isNaN(resultCount)) throw new Error('You must input your resultCount as an integer');
         let results = {};
         let url = this.URL;
-        url += this._search + 'query=' + type + '=' + query;
+        url += this.TYPE.SEARCH + 'query=' + type + '=' + query;
         request.get(url, (err, req, body) => {
             if (err) throw err;
             for (let i = 0; i < resultCount; i++) {
