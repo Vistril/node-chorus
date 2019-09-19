456241488ab9b3b476e7ad5d05a466dda39a8ca9
Vistril <38549907+Vistril@users.noreply.github.com>
2019-09-19 15:21:08 -0600

    strings

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const request = require('request');
 class ChorusAPI {
     constructor() {
         this.URL = 'http://chorus.fightthe.pw/';
-        this.search = 'api/search';
+        this.search = 'api/search?';
         this.count = 'api/count';
         this.random = 'api/random';
     }
 
     search(type, query, resultCount) {
         let results = {};
-        let url = URL;
-        url += '/api/search?query=' + type + '=' + searchQuery;
+        let url = this.URL;
+        url += this.search + 'query=' + type + '=' + searchQuery;
         request.get(url, (err, req, body) => {
           for (let i = 0; i < resultCount; i++) {
               results[resultCount] = {
